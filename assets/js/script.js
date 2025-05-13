2f74f2600ebc621b32b684287a22be09635596f0
Ishmael Sunday <okayishmael@gmail.com>
2025-05-12 20:48:19 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//copyright year automatically update every year
+const today = new Date();
+const year = today.getFullYear();
+const copyRightYear = document.getElementById("year");
+copyRightYear.innerHTML = year;
